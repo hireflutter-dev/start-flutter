a96eff0b79db99e3c3dea9e3b35e035cf7334735
chethanprabhakar <chethan@tritonetech.com>
2021-07-03 16:39:01 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/hf_flutter_starter_kit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hf_flutter_starter_kit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:hf_flutter_starter_kit/src/app.dart';
+
+void main() {
+  runApp(App());
+}
