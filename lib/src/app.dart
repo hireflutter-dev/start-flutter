92b3c486091936f0f21ba5e2597fc8fc6d63cd8f
chethanprabhakar <chethan@tritonetech.com>
2021-07-04 11:56:42 +0530

    build ios app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:hf_flutter_starter_kit/src/app/feature/splash/splash_screen.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('app'),
+    return MaterialApp(
+      title: 'Flutter Starter Kit',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
+      ),
+      home: SplashScreen(),
     );
   }
 }
