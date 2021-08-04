fcc49d91054b3af898447df28aac01135bc19e53
Abhishek01039 <abhishekghaskata1999@gmail.com>
2021-08-04 13:15:50 +0530

    feat: update file structure

--- lib/src/app/router/router.dart
+++ lib/src/app/router/router.dart
@@ -1,6 +1,6 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:hf_flutter_starter_kit/src/app/feature/authentication/view/login_screen.dart';
-import 'package:hf_flutter_starter_kit/src/app/feature/authentication/view/login_verify_screen.dart';
+import 'package:hf_flutter_starter_kit/src/app/feature/authentication/presentation/login_screen.dart';
+import 'package:hf_flutter_starter_kit/src/app/feature/authentication/presentation/login_verify_screen.dart';
 import 'package:hf_flutter_starter_kit/src/app/feature/home/presentation/home_screen.dart';
 import 'package:hf_flutter_starter_kit/src/app/feature/splash/splash_screen.dart';
 
