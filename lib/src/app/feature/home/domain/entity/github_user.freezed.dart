cc76dfea750ca39b40da349c4ec2996aeeb710cd
Jigar Patel <japatel225@gmail.com>
2022-04-05 11:19:17 +0530

    Changes by code generation

--- lib/src/app/feature/home/domain/entity/github_user.freezed.dart
+++ lib/src/app/feature/home/domain/entity/github_user.freezed.dart
@@ -1,5 +1,6 @@
+// coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
 
 part of 'github_user.dart';
 
@@ -150,7 +151,7 @@ class _$_GithubUser implements _GithubUser {
   _$_GithubUser({this.id, this.login, this.avatar_url, this.organizations_url});
 
   factory _$_GithubUser.fromJson(Map<String, dynamic> json) =>
-      _$_$_GithubUserFromJson(json);
+      _$$_GithubUserFromJson(json);
 
   @override
   final int? id;
@@ -197,7 +198,7 @@ class _$_GithubUser implements _GithubUser {
 
   @override
   Map<String, dynamic> toJson() {
-    return _$_$_GithubUserToJson(this);
+    return _$$_GithubUserToJson(this);
   }
 }
 
